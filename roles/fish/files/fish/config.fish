cf130cdc9bb0c9288e988257ce5fb42e3c1b2672
gntsldr <jimmy_superman_mcvay@msn.com>
2025-12-02 22:17:53 -0500

    Update config

--- roles/fish/files/fish/config.fish
+++ roles/fish/files/fish/config.fish
@@ -1,9 +1,37 @@
+function fish_title
+    if set -q SSH_CONNECTION
+        hostname
+    end
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+set -g fish_greeting
 
 starship init fish | source
 
 fzf --fish | source
 
 zoxide init --cmd cd fish | source
+
+abbr -a egrep 'egrep --color=auto'
+abbr -a fgrep 'fgrep --color=auto'
+abbr -a grep 'grep --color=auto'
+#abbr -a ls 'ls --color=auto'
+abbr -a ls 'eza --color=always --icons=auto --group-directories-first --group'
+abbr -a ll 'eza -la --color=always --icons=auto --group-directories-first --group'
+abbr -a tree 'eza --tree --color=always --icons=auto'
+abbr -a df 'dysk'
+abbr -a dysk 'dysk --units binary'
+abbr -a du 'dust'
+abbr -a cat 'bat --paging=never'
+abbr -a neofetch 'fastfetch -c ~/.config/fastfetch/presets/neofetch.jsonc'
+abbr -a k 'kubectl'
+
+# Autostart Zellij
+# if set -q ZELLIJ
+# else
+#   zellij
+# end
